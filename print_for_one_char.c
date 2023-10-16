10c653a061e346fc8b28070ccc54ff883aef42ca
bellacocho123 <isabelsky59@gmail.com>
2023-10-16 14:49:32 -0700

    task 0 print one char

--- print_for_one_char.c
+++ print_for_one_char.c
@@ -1,33 +1,16 @@
 #include "main.h"
-#include <stdio.h>
 
-int _printf(const char *format, ...) {
-  va_list args;
-  va_start(args, format);
+/**
+ * print_for_one_char - to function to print one char
+ * @element: the va_list element passed in from _printf.c
+ * Return: the number of charactes in the string
+ */
+int print_for_one_char(va_list element)
+{
+	char s;
 
-  int count = 0;
-  while (*format != '\0') {
-    if (*format == '%') {
-      switch (*++format) {
-        case 'c':
-          count += putchar(va_arg(args, int));
-          break;
-        case 's':
-          count += fputs(va_arg(args, char *), stdout);
-          break;
-        case '%':
-          count += putchar('%');
-          break;
-        default:
-          fprintf(stderr, "Unknown format specifier '%c'\n", *format);
-          return -1;
-      }
-    } else {
-      count += putchar(*format);
-    }
-    format++;
-  }
+	s = va_arg(element, int);
+	_putchar(s);
 
-  va_end(args);
-  return count;
+	return (1);
 }
