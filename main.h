797c4d5da0d1224e3410e092f244662fcad86825
bellacocho123 <isabelsky59@gmail.com>
2023-10-16 16:26:20 -0700

    main.h ,prototypes

---
+++ main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* Importing standard C lib */
+
+#include <unistd.h>
+#include <stdarg.h>
+
+/* function prototypes */
+int _printf(const char *format, ...);
+int _puts(char *string);
+
+#endif /* MAIN */
