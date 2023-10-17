440c686d5ac198e5d2dae7ac2882445f41f425cb
bellacocho123 <isabelsky59@gmail.com>
2023-10-16 23:33:43 -0700

    _putchar.c

--- _putchar.c
+++ _putchar.c
@@ -2,13 +2,12 @@
 
 /**
  * _putchar - writes the character c to stdout
- * @z: The character to print
- * Description - function to use stdio and prints first
- * character while pointing to z
- * Return: 0 on success
+ * @c: The character to print
+ *
+ * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-int _putchar(char z)
+int _putchar(char c)
 {
 	return (write(1, &c, 1));
 }
